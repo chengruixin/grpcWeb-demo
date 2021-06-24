820fd8ee99dc2743dea5618003e67a7bf394e15c
chengruixin <chengruixin_2010@163.com>
2021-06-25 06:06:43 +0800

    complete client side code refactor

---
+++ client/webpack.config.js
@@ -0,0 +1,15 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'index.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  devServer: {
+    contentBase: path.join(__dirname, 'public'),
+    compress: true,
+    port: 3000,
+  },
+  mode : "development"
+};
