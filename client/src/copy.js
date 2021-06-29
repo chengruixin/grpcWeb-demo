08396fac445072b2ad3a32294ae3f29e12e7d39e
chengruixin <chengruixin_2010@163.com>
2021-06-29 18:06:35 +0800

    udpate

--- client/src/copy.js
+++ client/src/copy.js
@@ -11,7 +11,8 @@ const localVideo = document.querySelector('video#video1');
 const remoteVideo = document.querySelector('video#video2');
 const streamingBtn = document.querySelector('button#start');
 streamingBtn.onclick = () => {
-    mediaRecorder = new MediaRecorder(window.stream, { mimeType: 'video/webm; codecs="vp8, opus"' });
+    // mediaRecorder = new MediaRecorder(window.stream, { mimeType: 'video/webm; codecs="vp8, opus"' });// default
+    mediaRecorder = new MediaRecorder(window.stream, { mimeType: 'video/webm; codecs="avc1.42E01E"' });
     
     mediaRecorder.ondataavailable = event => {
         if (event.data && event.data.size > 0) {
@@ -19,10 +20,11 @@ streamingBtn.onclick = () => {
         }
     };
     mediaRecorder.start(1); // time slice 1ms
+    console.log(mediaRecorder.mimeType)
 };
 
 navigator.mediaDevices.getUserMedia({
-    audio : true,
+    // audio : true,
     video : true
 }).then( stream => {
     window.stream = stream;
@@ -31,7 +33,9 @@ navigator.mediaDevices.getUserMedia({
 
 mediaSource.addEventListener('sourceopen', function (e) {
    
-    const mimeCodec = 'video/webm; codecs="vp8, opus"';
+    //const mimeCodec = 'video/webm; codecs="vp8, opus"';// default
+    const mimeCodec = 'video/mp4; codecs="avc1.42E01E"';
+
     sourceBuffer = mediaSource.addSourceBuffer(mimeCodec);
     
     sourceBuffer.addEventListener('updateend', function () {
@@ -53,3 +57,37 @@ socket.on('videoframes', function (data) {
         }
     }
 });
+
+{
+
+    const codecs = [
+        'video/mp4; codecs=avc1.42E01E',
+        'video/mp4; codecs=H264',
+        'video/mp4; codecs=h264',
+        'video/mp4; codecs=avc1',
+        'video/mp4; codecs=vp8',
+        'video/webm; codecs=avc1.42E01E',
+        'video/webm; codecs=H264',
+        'video/webm; codecs=h264',
+        'video/webm; codecs=avc1',
+        'video/webm; codecs=vp8'
+    ]
+
+    for(let codec of codecs){
+        if(MediaRecorder.isTypeSupported(codec)){
+            console.log("MediaRecorder supports :", codec);
+        } else {
+            console.log("MediaRecorder does not support :", codec);
+
+        }
+    }
+
+    // for(let codec of codecs){
+    //     if(MediaSource.isTypeSupported(codec)){
+    //         console.log("MediaSource supports :", codec);
+    //     } else {
+    //         console.log("MediaSource does not support :", codec);
+
+    //     }
+    // }
+}
